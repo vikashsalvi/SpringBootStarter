7dfb92e07776611d984eecb7ef3b8b623a63ed83
Vikash Manharlal Salvi <vk299944@dal.ca>
2020-01-17 19:37:27 -0400

    Commiting Spring Boot basic app

---
+++ src/test/java/com/example/accessingmysql/AccessingMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
